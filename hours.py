c0bc5fe14dd1171ae6433069eda7dcfeacf408b0
Mattijs Ugen <akaidiot@users.noreply.github.com>
2018-02-14 19:39:18 +0100

    Default action to log, print args

--- hours.py
+++ hours.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 
 def log_hours(name, date, hours):
     pass
@@ -13,5 +15,26 @@ def show_range(start, end):
     pass
 
 
+def do_log(arguments):
+    print('log', *arguments)
+
+
+def do_show(arguments):
+    print('show', *arguments)
+
+
+def main(arguments):
+    actions = {
+        'log': do_log,
+        'show': do_show,
+    }
+
+    action = 'log'
+    if arguments[0] in actions:
+        action = arguments.pop(0)
+
+    actions[action](arguments)
+
+
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
