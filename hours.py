ce4743147b743139342c11076229b80e25311584
Mattijs Ugen <akaidiot@users.noreply.github.com>
2018-02-14 19:29:34 +0100

    Outline functions for hours

---
+++ hours.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def log_hours(name, date, hours):
+    pass
+
+
+def show_day(date):
+    pass
+
+
+def show_range(start, end):
+    pass
+
+
+if __name__ == '__main__':
+    pass
