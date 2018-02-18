aa6966ef8eb839256901cc0da31765da746ede40
Mattijs Ugen <akaidiot@users.noreply.github.com>
2018-02-18 12:07:23 +0100

    Move functions to Session

--- hours.py
+++ hours.py
@@ -76,9 +76,9 @@ class Session:
 
     def run(self, arguments):
         actions = {
-            'log': do_log,
-            'show': do_show,
-            'alias': create_alias,
+            'log': self.run_log,
+            'show': self.run_show,
+            'alias': self.create_alias,
         }
 
         action = 'log'
@@ -90,120 +90,112 @@ class Session:
 
             actions[action](self.database, arguments)
 
+    def select_day(self, argument):
+        if argument in DAYS:
+            return DAYS[argument]
 
-def select_day(argument):
-    if argument in DAYS:
-        return DAYS[argument]
+        if DATE_PATTERN.match(argument):
+            year, month, day = argument.split('-')
+            return date(int(year), int(month), int(day))
 
-    if DATE_PATTERN.match(argument):
-        year, month, day = argument.split('-')
-        return date(int(year), int(month), int(day))
+        raise ValueError('not a day: {}'.format(argument))
 
-    raise ValueError('not a day: {}'.format(argument))
-
-
-def to_name(database, argument):
-    cursor = database.execute(
-        """
-            SELECT name FROM aliases WHERE alias = ?
-        """,
-        (argument,)
-    )
-    name = cursor.fetchone()
-    if name:
-        return name[0]
-    else:
-        return argument
-
-
-def log_hours(database, name, day, hours):
-    database.execute(
-        """
-            INSERT INTO hours (name, day, hours) VALUES (?, ?, ?)
-        """,
-        (name, day, hours)
-    )
-
-
-def do_log(database, arguments):
-    assert 2 <= len(arguments) <= 3
-
-    day = TODAY
-    name = hours = None
-
-    for argument in arguments:
-        if argument in DAYS or DATE_PATTERN.match(argument):
-            day = select_day(argument)
-        elif HOURS_PATTERN.match(argument):
-            hours = float(argument.replace(',', '.'))
+    def to_name(self, argument):
+        cursor = self.database.execute(
+            """
+                SELECT name FROM aliases WHERE alias = ?
+            """,
+            (argument,)
+        )
+        name = cursor.fetchone()
+        if name:
+            return name[0]
         else:
-            name = to_name(database, argument)
+            return argument
 
-    assert None not in (name, day, hours)
+    def log_hours(self, name, day, hours):
+        self.database.execute(
+            """
+                INSERT INTO hours (name, day, hours) VALUES (?, ?, ?)
+            """,
+            (name, day, hours)
+        )
 
-    return log_hours(database, name, day, hours)
+    def run_log(self, arguments):
+        assert 2 <= len(arguments) <= 3
 
+        day = TODAY
+        name = hours = None
 
-def show_day(database, day):
-    cursor = database.execute(
-        """
-            SELECT name, SUM(hours) FROM hours WHERE day = ? GROUP BY name ORDER BY name
-        """,
-        (day,)
-    )
+        for argument in arguments:
+            if argument in DAYS or DATE_PATTERN.match(argument):
+                day = self.select_day(argument)
+            elif HOURS_PATTERN.match(argument):
+                hours = float(argument.replace(',', '.'))
+            else:
+                name = self.to_name(argument)
 
-    print(tabulate(cursor.fetchall(),
-                   headers=('', day.isoformat())))
+        assert None not in (name, day, hours)
 
+        return self.log_hours(name, day, hours)
 
-def show_range(database, start, end):
-    days = [(start + timedelta(days=offset)).isoformat() for offset in range((end - start).days + 1)]
+    def show_day(self, day):
+        cursor = self.database.execute(
+            """
+                SELECT name, SUM(hours) FROM hours WHERE day = ? GROUP BY name ORDER BY name
+            """,
+            (day,)
+        )
 
-    cursor = database.execute(
-        """
-            SELECT name, day, SUM(hours) FROM hours WHERE day >= ? AND day <= ? GROUP BY name, day ORDER BY name, day
-        """,
-        (start, end)
-    )
+        print(tabulate(cursor.fetchall(),
+                       headers=('', day.isoformat())))
 
-    data = {(name, day): hours for (name, day, hours) in cursor.fetchall()}
-    names = sorted({name for (name, day) in data.keys()})
+    def show_range(self, start, end):
+        days = [(start + timedelta(days=offset)).isoformat() for offset in range((end - start).days + 1)]
 
-    print(tabulate([[name] + [data.get((name, day)) for day in days] for name in names],
-                   headers=[''] + days))
+        cursor = self.database.execute(
+            """
+                SELECT name, day, SUM(hours) FROM hours WHERE day >= ? AND day <= ? GROUP BY name, day ORDER BY name, day
+            """,
+            (start, end)
+        )
 
+        data = {(name, day): hours for (name, day, hours) in cursor.fetchall()}
+        names = sorted({name for (name, day) in data.keys()})
 
-def do_show(database, arguments):
-    assert 0 <= len(arguments) <= 2
+        print(tabulate([[name] + [data.get((name, day)) for day in days] for name in names],
+                       headers=[''] + days))
 
-    if len(arguments) == 2:
-        if arguments == ['last', 'week']:
-            start = WEEK_START - timedelta(days=7)
-            end = WEEK_START - timedelta(days=1)
+    def run_show(self, arguments):
+        assert 0 <= len(arguments) <= 2
+
+        if len(arguments) == 2:
+            if arguments == ['last', 'week']:
+                start = WEEK_START - timedelta(days=7)
+                end = WEEK_START - timedelta(days=1)
+            else:
+                start = self.select_day(arguments[0])
+                end = self.select_day(arguments[1])
+        elif len(arguments) == 1 and arguments[0] != 'week':
+            start = end = self.select_day(arguments[0])
         else:
-            start = select_day(arguments[0])
-            end = select_day(arguments[1])
-    elif len(arguments) == 1 and arguments[0] != 'week':
-        start = end = select_day(arguments[0])
-    else:
-        start = WEEK_START
-        end = WEEK_START + timedelta(days=6)
+            start = WEEK_START
+            end = WEEK_START + timedelta(days=6)
 
-    if start == end:
-        return show_day(database, start)
-    else:
-        return show_range(database, *sorted((start, end)))
+        if start == end:
+            return self.show_day(start)
+        else:
+            return self.show_range(*sorted((start, end)))
 
+    def create_alias(self, arguments):
+        assert len(arguments) == 2
 
-def create_alias(database, arguments):
-    assert len(arguments) == 2
-
-    database.execute(
-        """
-            INSERT OR REPLACE INTO aliases (alias, name) VALUES (?, ?)
-        """,
-        arguments
-    )
+        self.database.execute(
+            """
+                INSERT OR REPLACE INTO aliases (alias, name) VALUES (?, ?)
+            """,
+            arguments
+        )
 
 
 if __name__ == '__main__':
